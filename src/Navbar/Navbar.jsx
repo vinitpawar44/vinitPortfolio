4ca0f1f8895ef06c680ebf382e983054d0551e40
vinitpawar44 <vinitpawar958@gmail.com>
2024-11-15 20:00:38 +0530

    Update Navbar.jsx

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
           <li ><a href="#projects">Projects</a></li>
           <li ><a href="#education">Education</a></li>
           <li  ><a href="#contact">Contact</a></li>
-          <li ><a className="github" href="https://github.com/vruttik223">Github Profile</a></li>
+         
         </ul>
       </div>
       <div onClick={() => setRespnav(!respnav)} className="responsive">

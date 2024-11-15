19d2ced7a644930ad33e80b9d92087e4b1da6892
vinitpawar44 <vinitpawar958@gmail.com>
2024-11-15 18:20:01 +0530

    update image

--- src/Work/Work.jsx
+++ src/Work/Work.jsx
@@ -7,7 +7,7 @@ import ecom from "../images/ecom.png";
 // import staybnb from "../images/staybnb.png";
 import capcha from "../images/capcha.png";
 import xzoom from "../images/xzoom.png";
-import profile from './src/Work/Capture.JPG';
+import profile from '../images/Capture.png';
 
 const projects = [
     {
